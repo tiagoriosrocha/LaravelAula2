803b26d0072e15268becb82d8fc4f04c1e696374
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2018-09-17 16:39:11 -0300

    relacionamentos prontos

--- database/migrations/2018_08_01_145304_add_mensagens_table.php
+++ database/migrations/2018_08_01_145304_add_mensagens_table.php
@@ -18,7 +18,14 @@ class AddMensagensTable extends Migration
             $table->string('titulo');   //título da mensagem
             $table->string('texto');    //texto da mensagem
             $table->string('autor');    //autor da mensagem
+            $table->integer('user_id')->unsigned(); //guarda o id de quem cadastra a msg.
+            $table->integer('atividade_id')->unsigned(); //guarda o id da atividade
             $table->timestamps();       //registro created_at e updated_at
+        });
+
+        Schema::table('mensagens', function($table){
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('atividade_id')->references('id')->on('atividades');
         });
     }
 
--- resources/views/atividade/list.blade.php
+++ resources/views/atividade/list.blade.php
@@ -42,3 +42,5 @@
 @auth
   <p><a href="/atividades/create">Criar novo registro</a></p>
 @endauth
+
+
